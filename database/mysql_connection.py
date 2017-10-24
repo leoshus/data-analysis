65eadd732c9d2c1f4080eccc614f192acf20f5f3
shangyindong <shangyindong@corp.netease.com>
2017-10-24 19:23:45 +0800

    modify

--- database/mysql_connection.py
+++ database/mysql_connection.py
@@ -1,5 +1,37 @@
 #-*- coding=utf-8 -*-
 
+import traceback
+
+
 class MySQLConnection(object):
 
-    def __init__(self):
+    def __init__(self, conn):
+        self.__conn = conn
+        self.__session = self.__conn.cursor()
+        pass
+
+    def close(self):
+        try:
+            self.__session.close()
+            self.__conn.close()
+        except:
+            traceback.print_exc()
+        pass
+
+    def execute(self, sql):
+        self.__session.execute(sql)
+        ret = self.__session.fetchall()
+        return ret
+
+
+    def beginTransaction(self):
+        self.__conn.autocommit(0)
+        pass
+
+    def endTransaction(self, option='commit'):
+        if option == 'commit':
+            self.__conn.commit()
+        else:
+            self.__conn.rollback()
+        pass
+
--- database/mysql_pool.py
+++ database/mysql_pool.py
@@ -1,19 +1,25 @@
 #-*- coding=utf-8 -*-
+
 import MySQLdb
+import sys
 from MySQLdb.cursors import DictCursor
 
 from mysql_config import DATABASE_CONFIG
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from mysql_connection import MySQLConnection
+from DBUtils.PooledDB import PooledDB
 
-def singleton(cls,*args,**kw):
+
+def singleton(cls, *args, **kw):
     instance = {}
 
-    def _wrapper(cls):
+    def _wrapper():
         if cls not in instance:
-            instance[cls]=cls(*args,**kw)
+            instance[cls] = cls(*args, **kw)
         return instance[cls]
     return _wrapper
+
 
 @singleton
 class SQLAlchemyPool(object):
@@ -33,11 +39,31 @@ class SQLAlchemyPool(object):
         return session
         pass
 
+
 @singleton
 class MySQLPool(object):
     __instance = None
 
-    def getSingleConnection():
+    def __init__(self, min_conn=2):
+        self.__host = DATABASE_CONFIG['HOST']
+        self.__user = DATABASE_CONFIG['USER']
+        self.__password = DATABASE_CONFIG['PASSWORD']
+        self.__database = DATABASE_CONFIG['DATABASE']
+        self.__min_conn = min_conn
+        self.__pool = PooledDB(
+            MySQLdb,
+            self.__min_conn,
+            host=self.__host,
+            user=self.__user,
+            passwd=self.__password,
+            db=self.__database,
+            charset='utf-8',
+            use_unicode=True,
+            cursorclass=DictCursor
+        )
+
+    @staticmethod
+    def getSingleConnection(self):
         conn = MySQLdb.connect(
             DATABASE_CONFIG['HOST'],
             DATABASE_CONFIG['USER'],
@@ -48,3 +74,15 @@ class MySQLPool(object):
             cursorclass=DictCursor
         )
         wrapped_conn = MySQLConnection(conn)
+        return wrapped_conn
+    pass
+
+    def getConnection(self):
+        try:
+            conn = self.__pool.connection()
+            wrapped_conn = MySQLConnection(conn)
+            return wrapped_conn
+        except MySQLdb.Error as e:
+            sys.stderr.write("Error %d:%s\n" % (e.args[0], e.args[1]))
+            return None
+    pass
